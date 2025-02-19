d58116e16c4b3c71175c877c097d3afba6b71da9
sasha.deroubaix <sasha.deroubaix@cyna-it.fr>
2025-02-19 15:09:12 +0100

    update

--- 04/ex04/ft_putnbr_base.c
+++ 04/ex04/ft_putnbr_base.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define PLUS_SIGN 43
+#define MINUS_SIGN 45
+
 int ft_strlen(char *str){
   int i = 0;
 
@@ -9,18 +12,54 @@ int ft_strlen(char *str){
   return i;
 }
 
+int ft_has_repeated_char(char *base){
+    int i = 0;
+    int j = 0;
+
+    for (i = 0; base[i] != '\0'; i++) {
+        for (j = 0; base[j] != '\0'; j++){
+            if (base[i] == base[j] && i != j)
+                return 1;
+        } 
+    }
+
+    return 0;
+}
+
+int ft_has_sign_char(char *base){
+    int i = 0;
+
+    for (i = 0; base[i] != '\0'; i++){
+        if (base[i] == PLUS_SIGN || base[i] == MINUS_SIGN) 
+            return 1;
+    }
+
+    return 0;
+}
+
+int ft_is_base_valid(char *base, ){
+    int has_sign = ft_has_sign_char(base);
+    int has_repeated = ft_has_repeated_char(base);
+
+    if (!has_sign && !has_repeated && )
+}
+
 void ft_putnbr_base(int nbr, char *base){
   int base_len = ft_strlen(base);
 
-  if (base_len <= 1)
-    
-  
+  int repeated_char = ft_has_repeated_char(base);
+
+  if (base_len > 1 && !repeated_char)
+      printf("base valide\n");
+  else
+      printf("Base non valide\n");
+
 }
 
 int main(){
   int nbr = 42;
 
-  ft_putnbr_base(42, "0123456789ABCDEF");
+  ft_putnbr_base(42, "0123456789ABCDEFB");
 
   return 0;
 }
