2e4704ee181e153046076e27c57f9c115e0ea663
0x2brunbaix <sasha.deroubaix.dev@proton.me>
2025-02-19 23:19:11 +0100

    update

--- 04/ex04/ft_putnbr_base.c
+++ 04/ex04/ft_putnbr_base.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #define PLUS_SIGN 43
 #define MINUS_SIGN 45
@@ -37,29 +38,39 @@ int ft_has_sign_char(char *base){
     return 0;
 }
 
-int ft_is_base_valid(char *base, ){
+int ft_is_base_valid(char *base, int base_len){
     int has_sign = ft_has_sign_char(base);
     int has_repeated = ft_has_repeated_char(base);
 
-    if (!has_sign && !has_repeated && )
+    if (!has_sign && !has_repeated && base_len > 1)
+        return 1;
+
+    return 0;
+}
+
+void ft_convert_number_to_base(int nbr, char *base, int base_len){
+    if (nbr < 0) {
+        write(1, "-", 1);
+        nbr = -nbr;
+    }
+    if (nbr >= base_len)
+        ft_convert_number_to_base(nbr/base_len, base, base_len);
+    write(1, &base[nbr % base_len], 1);
 }
 
 void ft_putnbr_base(int nbr, char *base){
   int base_len = ft_strlen(base);
 
-  int repeated_char = ft_has_repeated_char(base);
-
-  if (base_len > 1 && !repeated_char)
-      printf("base valide\n");
+  if (ft_is_base_valid(base, base_len))
+      printf("Base valide\n");
   else
       printf("Base non valide\n");
 
+  ft_convert_number_to_base(nbr, base, base_len);
 }
 
 int main(){
-  int nbr = 42;
-
-  ft_putnbr_base(42, "0123456789ABCDEFB");
+  ft_putnbr_base(24, "01");
 
   return 0;
 }
